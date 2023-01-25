6bee8dadde8e8cec7fcf8c88a134e4c954cb7ae1
Amanda So <soamanda95@gmail.com>
2023-01-24 20:40:18 -0800

    add login form template with bulma css style

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,29 @@ const Login = () => {
   return (
     <div>
       <h1>Login</h1>
+      <form>
+        <div className="field">
+          <label className="label">Username</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="johnsmith" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input
+              className="input"
+              type="password"
+              placeholder="Enter Password Here"
+            />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button">Login</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
