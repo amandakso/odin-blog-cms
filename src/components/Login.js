2f39320eb06169939101d0591d9ed395a62f7516
Amanda So <soamanda95@gmail.com>
2023-01-25 15:29:01 -0800

    send http post request to login user

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:3000/blog/login", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+      let resJson = await res.json();
+
+      if (res.status === 200) {
+        if (resJson.error) {
+          setError(resJson.error);
+        }
+        if (resJson.message) {
+          alert(resJson.message);
+        }
+        if (resJson.token) {
+          setUsername("");
+          setPassword("");
+          setError(null);
+          sessionStorage.setItem("token", resJson.token);
+          alert(`Welcome ${resJson.username}!`);
+          navigate("/");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
@@ -8,7 +52,13 @@ const Login = () => {
         <div className="field">
           <label className="label">Username</label>
           <div className="control">
-            <input className="input" type="text" placeholder="johnsmith" />
+            <input
+              className="input"
+              type="text"
+              value={username}
+              placeholder="johnsmith"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
         </div>
         <div className="field">
@@ -17,16 +67,21 @@ const Login = () => {
             <input
               className="input"
               type="password"
+              value={password}
               placeholder="Enter Password Here"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
         </div>
         <div className="field is-grouped">
           <div className="control">
-            <button className="button">Login</button>
+            <button className="button" onClick={(e) => loginUser(e)}>
+              Login
+            </button>
           </div>
         </div>
       </form>
+      {error ? <p>{error}</p> : null}
     </div>
   );
 };
