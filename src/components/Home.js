197643a6c945fb4e90bf4c54a7b664ef71c8ec38
Amanda So <soamanda95@gmail.com>
2023-01-30 15:04:29 -0800

    send http get request for user posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Navbar from "./Navbar";
 
 const Home = () => {
-  return <Navbar />;
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const getPosts = async () => {
+      let token = sessionStorage.getItem("token");
+      console.log(token);
+      if (!token) {
+        alert("Error with user access");
+      } else {
+        try {
+          const res = await fetch(`http://localhost:3000/blog/posts/author`, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `bearer ${token}`,
+            },
+          });
+          let resJson = await res.json();
+          setData(resJson);
+          console.log(resJson);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
